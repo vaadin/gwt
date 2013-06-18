810422146ef17cd830448de9e3f6ad6a8c1edfd6
Brian Slesinsky <skybrian@google.com>
2013-06-18 14:19:30 -0700

    Revert "Revert "Revert "Makes GWT.runAsync to always run async."""
    This reverts commit a347efd07ba5d18f4750f8fcbfa3c6e7b1c87363.

--- user/src/com/google/gwt/core/client/GWT.java
+++ user/src/com/google/gwt/core/client/GWT.java
@@ -15,7 +15,6 @@
  */
 package com.google.gwt.core.client;
 
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.core.client.impl.Impl;
 
 /**
@@ -259,18 +258,14 @@ public final class GWT {
    */
   @SuppressWarnings("unused") // parameter will be used following replacement
   public static void runAsync(Class<?> name, RunAsyncCallback callback) {
-    runAsync(callback);
+    callback.onSuccess();
   }
 
   /**
    * Run the specified callback once the necessary code for it has been loaded.
    */
-  public static void runAsync(final RunAsyncCallback callback) {
-    Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-      @Override public void execute() {
-        callback.onSuccess();
-      }
-    });
+  public static void runAsync(RunAsyncCallback callback) {
+    callback.onSuccess();
   }
 
   /**
